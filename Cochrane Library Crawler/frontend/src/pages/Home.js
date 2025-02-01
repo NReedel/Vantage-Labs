d3d0f16987e42760da46f0574e7ba1aa6d210241
NReedel <125296416+NReedel@users.noreply.github.com>
2025-02-01 16:22:09 -0500

    Delete Cochrane Library Crawler/frontend directory

--- Cochrane Library Crawler/frontend/src/pages/Home.js
+++
@@ -1,8 +0,0 @@
-// Might not need
-// fetch reviews from the backend.
-useEffect(() => {
-	fetch("http://localhost:8080/api/reviews")
-	  .then(response => response.json())
-	  .then(data => setReviews(data))
-	  .catch(error => console.error("Error fetching reviews:", error));
-  }, []);
