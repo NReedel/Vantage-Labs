63a9705802670fbc5560e37d4f6cee4e709a833b
NReedel <125296416+NReedel@users.noreply.github.com>
2025-02-01 16:23:29 -0500

    Add files via upload

---
+++ Cochrane Library Crawler/frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Home from "./pages/Home"; // Import the Home component
+
+const App = () => {
+    return (
+        <div>
+            <h1 className="header">Cochrane Library Reviews</h1>
+            <Home />
+        </div>
+    );
+};
+
+export default App;
